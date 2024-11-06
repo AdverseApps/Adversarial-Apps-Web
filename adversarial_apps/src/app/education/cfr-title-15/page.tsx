a5cc37390cb14a49cb1b3cd84a75b3a145630dd7
Oluwadamilola Ogboja <damiogb@gmail.com>
2024-11-05 21:51:23 -0500

    completing CFR 15 page
    Adding initial information to our CFR 15 Page

--- adversarial_apps/src/app/education/cfr-title-15/page.tsx
+++ adversarial_apps/src/app/education/cfr-title-15/page.tsx
@@ -1,3 +1,20 @@
 export default function page(){
-    return (<p>CFR Title 15 Page</p>);
+    return (
+    <>
+        <h1 className="text-6xl font-bold pl-5"><br />CFR Title 15</h1>
+        <h4 className="text-xl font-bold pl-5"><br />What is Title 15</h4>
+        <p className = "pl-5">A CFR stands for Code of Federal Regulations and contains 
+            all the codes and regulations published by the government 
+            from various departments.</p>
+
+        <p className = "pl-5"><br />What this law does is that it extends SBIR and STTR awards 
+            till 2025 instead of ending in 2022 which is what it was set to originally. 
+            More importantly to our project, it also established the Due Diligence Program. 
+            This program was established to assess the security risks associated with cybersecurity 
+            practices, patents, foreign adversaries and ownership, employees, and more for small businesses seeking awards 
+            through these programs for each federal head in charge of that process. It further requires that each small business 
+            disclose certain information in regard to foreign adversaries such as the ownership percentage for foreign adversaries, 
+            patent processing in foreign countries, etc.</p>
+    </>
+    );
 }
