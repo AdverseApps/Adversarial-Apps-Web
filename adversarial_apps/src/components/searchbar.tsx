81512b0370d449da3e30805db443fec9dbad877a
Oluwadamilola Ogboja <damiogb@gmail.com>
2024-11-08 21:25:47 -0500

    changed routing and conditions
    - made it so searching always routes to the search page first before displaying results no matter what path the search was made from
    
    - changed the search function to only search once enter was clicked instead of off of any change

--- adversarial_apps/src/components/searchbar.tsx
+++ adversarial_apps/src/components/searchbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-
+import Image from 'next/image';
 
 export default function SearchBar({placeholder }: {placeholder: string}) {
     
@@ -15,22 +15,29 @@ export default function SearchBar({placeholder }: {placeholder: string}) {
         } else {
           params.delete('query');
         }
-        replace(`${pathname}?${params.toString()}`);
+        replace(`${"/search"}?${params.toString()}`);
       }
       return (
         <div className="relative flex">
           <label htmlFor="search" className="sr-only">
             Search
           </label>
+
+        
+
+
           <input
-      
+            
             className="block w-full rounded-3xl border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500 text-gray-500"
             placeholder={placeholder}
-            onChange={(e) => {
-              handleSearch(e.target.value);
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+              handleSearch(e.currentTarget.value);
+            }
             }}
             defaultValue={searchParams.get('query')?.toString()}
           />
+          
         </div>
       );
     }
