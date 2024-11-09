3191ddf96bdc9d3b585dd73b9cd7430dc4eb6dc3
Oluwadamilola Ogboja <damiogb@gmail.com>
2024-11-08 21:30:42 -0500

    imported component and added comments
    descriptive comments added
    
    imported "SearchResultsHandler" for client side state management

--- adversarial_apps/src/app/search/page.tsx
+++ adversarial_apps/src/app/search/page.tsx
@@ -1,6 +1,5 @@
-
 import SearchBar from '@/components/searchbar';
-
+import SearchResultsHandler from '@/components/SearchResultsHandler';
 import { Suspense } from 'react';
 
 export default function  Page({
@@ -12,27 +11,26 @@ export default function  Page({
     };
   }) {
     const query = searchParams?.query || '';
-    const currentPage = Number(searchParams?.page) || 1;
+    const currentPage = Number(searchParams?.page) || 1; //for whenever pagination is added
+
     return (
         <>
         <main>
             <h1 className={`text-4xl mt-5 ml-6`}>Search</h1>
 
-            
+          {/*search bar*/}
           <nav className="inset-0 flex items-center justify-center mt-9 gap-2 md:mt-8 ">
             <div className="w-2/5 rounded-sm"> 
             <SearchBar placeholder="Search..."  />
             </div>
           </nav>
-        <div className="flex justify-center">
-        <h3 className="text-left text-lg"><br /><br />Please input company name<br />
-        This will pull up a list of related companies<br />
-        Results will list <br />Company: <br />Country: <br />DFR14 found:
-        </h3>
-        </div>
+       
           <nav className="mt-5 flex w-full justify-center">
             {/* <Pagination totalPages={totalPages} /> */}
           </nav>
+
+           {/* Search Results and states handled here */}
+      <SearchResultsHandler query={query} currentPage={currentPage} />
           </main>
         </>
       );
