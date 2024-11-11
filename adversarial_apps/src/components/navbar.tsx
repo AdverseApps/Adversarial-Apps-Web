b1f5263f93c941ebcee27fd1530c39f96c321ba0
chrisrod1307 <rodriguez.christian325@gmail.com>
2024-11-11 15:01:54 -0500

    test commit

--- adversarial_apps/src/components/navbar.tsx
+++ adversarial_apps/src/components/navbar.tsx
@@ -6,6 +6,7 @@ export default function NavBar() {
         {/* FOR CHRISTIAN Finish making Navbar here 
         use the Link component in place of the <a> tag, 
         literally the same thing but Link is better*/}
+        <button>Education</button>
       </>
     );
 }
