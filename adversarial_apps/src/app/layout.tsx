77007f153cc382bb1ceb6eae3aa3121abfb8269b
chrisrod1307 <rodriguez.christian325@gmail.com>
2024-11-11 17:26:30 -0500

    feat: created navbar

--- adversarial_apps/src/app/layout.tsx
+++ adversarial_apps/src/app/layout.tsx
@@ -1,7 +1,5 @@
 import "./globals.css";
-import Image from "next/image";
-import Link from "next/link";
-import SearchBar from "@/components/searchbar";
+import NavBar from "@/components/navbar";
 
 export default function RootLayout({
   children,
@@ -11,36 +9,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-       <header 
-          style={{
-            backgroundColor: "#002759", //background color for the header
-            height:93
-          }}
-          className ="w-full drop-shadow-[0_4px_2px_rgba(255,255,255,1.0)] " //white drop shadow to separate header from main
-          
-          >
-            <figure className="relative h-auto w-auto">
-            <Link href = "/" className="inline-block"> 
-            <Image //our logo image that links to home page
-              src="/Adversarial_Apps_Logo.png"
-              width={302}
-              height={68}
-              alt="Adversarial Apps Logo"
-              className="ml-6"  // Adjust as needed
-            />
-            </Link>
-            </figure>
-
-           {/* SearchBar positioned in the top-right */}
-          <nav className="fixed top-0 right-0 mt-4 mr-8 w-1/6">
-            <SearchBar placeholder="Search..."/> {/* Control width with size */}
-          </nav>
-
-        </header>
-
-      
+        <NavBar />
         {children}
-       
       </body>
     </html>
   );
