f79eae3b76d942d16dad39e89e33704893d6c3d6
Conner Harbaugh <savvydolphin77@gmail.com>
2024-11-16 19:07:55 -0500

    Update API example page For Showing Report Call Updated the API example page to show how the generate report call works and what data it returns.

--- adversarial_apps/src/app/api-example-page/page.tsx
+++ adversarial_apps/src/app/api-example-page/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
   const [result, setResult] = useState(null);
 
   const sendDataToPython = async () => {
-    const data = { action: "obtain_cik_number", search_term: "Google" };
+    const data = { action: "generate_report", cik_number: "0000069499" };
     try {
       const response = await fetch('/api/call-python-api', {
         method: 'POST',
