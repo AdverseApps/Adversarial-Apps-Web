48d803ff8edc333ac9d00c6fb3ae3553805ac4cf
Oluwadamilola Ogboja <damiogb@gmail.com>
2024-12-26 07:35:13 -0500

    added aria labels to home page
    accessibility feature for screen readers

--- adversarial_apps/src/app/page.tsx
+++ adversarial_apps/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return(
     <div className="flex flex-col h-auto"> {/* Full height container, by doing h-auto it removes the scroll bar */}
          <br /> <br /> <br /> <br />
-            <main className="flex-grow">
+            <main className="flex-grow" aria-label="main-content">
                 {/*main text goes here*/}
                 
                 <h1 className="text-4xl text-center font-bold"> <br />
@@ -22,7 +22,7 @@ export default function Home() {
                 
                 <nav className="inset-0 flex items-center justify-center mt-9 gap-2 md:mt-8 ">
                     <div className="w-2/5 rounded-sm"> 
-                    <SearchBar placeholder="Search..." />
+                    <SearchBar placeholder="Search..." aria-label="Search for business partners" />
                     </div>
                 </nav>
 
@@ -32,19 +32,20 @@ export default function Home() {
         <footer className="text-white"> {/* dont add flex or it will mess upo sponsor logos* */}
             <h1 className="mb-1 text-xl font-semibold text-center text-white">Sponsors</h1>
             
-            <div className="flex justify-center items-center gap-4">
+            <div className="flex justify-center items-center gap-4" aria-label="Sponsor logos">
             <figure className="p-2">
                 <a
                  href="https://www.deftech.nc.gov" //link to Deftech website
                  target="_blank" //Opens the link in a new tab
                  rel="noopener noreferrer" // Recommended for security
                  className="cursor-pointer" //Adds pointer cursor on hover
+                 aria-label="Visit NCDeftech website"
                 >
                 <Image
                     src="/Deftech_Logo.png" //Deftech logo
                     width={200}
                     height={60}
-                    alt="Deftech Logo"
+                    alt="NCDeftech Logo"
                 />
                 </a>
             </figure>
@@ -55,6 +56,7 @@ export default function Home() {
                  target="_blank" //Opens the link in a new tab
                  rel="noopener noreferrer" // Recommended for security
                  className="cursor-pointer" //Adds pointer cursor on hover
+                 aria-label="Visit National Security Innovation Network website"
                 >
                 <Image
                     src="/NSIN_Logo.png" //NSIN Logo
