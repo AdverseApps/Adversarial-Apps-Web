81312eafc501fd61d8a1b5ab6d7e8bb3c85fdcdd
Oluwadamilola Ogboja <134186195+DamiOgboja@users.noreply.github.com>
2025-01-22 19:32:35 -0500

    updated spacing and added incoming feature text

--- adversarial_apps/src/app/company/[cik]/page.tsx
+++ adversarial_apps/src/app/company/[cik]/page.tsx
@@ -55,88 +55,95 @@ export default async function page ({ params }: CompanyDetailsProps){
       </div>
     </div>
   );
-}
-const { name, 
-        formerNames, 
-        address, 
-        street2,
-        city,
-        zipCode,
-        stateOrCountryDescription, 
-        stateOfIncorporation, 
-        mostRecentFilingDate, 
-        phone, 
-        website 
-} = result.company;
+  }
+  const { name, 
+          formerNames, 
+          address, 
+          street2,
+          city,
+          zipCode,
+          stateOrCountryDescription, 
+          stateOfIncorporation, 
+          mostRecentFilingDate, 
+          phone, 
+          website 
+  } = result.company;
 
 
-const formatDate = (date: string): string => {
-  const parsedDate = new Date(date);
-  return parsedDate.toLocaleDateString("en-US"); // Format: MM/DD/YYYY
-};
+  const formatDate = (date: string): string => {
+    const parsedDate = new Date(date);
+    return parsedDate.toLocaleDateString("en-US"); // Format: MM/DD/YYYY
+  };
 
   return(
-    
-  <div className="flex mt-6">
-    <div className="w-full max-w-xl text-left text-xl p-6 rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold mb-4">Company Details</h2>
-       <p>
-        <span className="font-semibold">Name:</span> {capitalizeWords(name) || "N/A"}
-      </p>
-      
-        {formerNames && formerNames.length > 0 && (
-          <div className=""><br />
-          <h3 className="text-lg font-semibold">Former Names:</h3>
-          <ul className="list-disc pl-5">
-            {formerNames.map((item: FormerName, index: number) => (
-              <li key={index}>
-                <span className="font-medium">{" "}{capitalizeWords(item.name)}</span> (From: {formatDate((item.fromDate))} To: {formatDate(item.toDate)})
-              </li>
-              ))}
-          </ul>
-        </div>
-      )}
-      <br />
-      <p>
-        <span className="font-semibold">Business Address:</span>  {capitalizeWords(address)?.replace(/,+$/, "") || "N/A"}
-        {street2 && `, ${capitalizeWords(street2).replace(/,+$/, "")}`}
-        {city && `, ${capitalizeWords(city).replace(/,+$/, "")}`}
-        {zipCode && `, ${zipCode}`}
-      </p>
-      <br />
-      <p>
-        <span className="font-semibold">State or Country:</span> {stateOrCountryDescription || "N/A"}
-      </p>
-      <br />
-      <p>
-        <span className="font-semibold">State of Incorporation:</span> {stateOfIncorporation || "N/A"}
-      </p>
-      <br />
-      <p>
-        <span className="font-semibold">Date of Last Filing:</span> {formatDate(mostRecentFilingDate) || "N/A"}
-      </p>
-      <br />
-      <p>
-        <span className="font-semibold">Phone:</span> {phone || "N/A"}
-      </p>
-      <br />
-      <p>
-        <span className="font-semibold">Website:</span>{" "}
-        {website ? (
-          <><a
-              href={website}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 underline"
-            >
-              {website}
-            </a>
-              <span className="font-semibold text-sm"> please note we do not verify any external website linked on this page, click on links at your own risk</span>
-            </>
-        ) : (
-          "N/A"
-        )}
+    <div className="flex mt-6 ">
+      {/* Left side */}
+      <div className="w-1/2 text-left text-xl p-6 rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold mb-4">Company Details</h2>
+        <p>
+          <span className="font-semibold">Name:</span> {capitalizeWords(name) || "N/A"}
         </p>
+        
+          {formerNames && formerNames.length > 0 && (
+            <div className=""><br />
+            <h3 className="text-lg font-semibold">Former Names:</h3>
+            <ul className="list-disc pl-5">
+              {formerNames.map((item: FormerName, index: number) => (
+                <li key={index}>
+                  <span className="font-medium">{" "}{capitalizeWords(item.name)}</span> (From: {formatDate((item.fromDate))} To: {formatDate(item.toDate)})
+                </li>
+                ))}
+            </ul>
+          </div>
+        )}
+        <br />
+        <p>
+          <span className="font-semibold">Business Address:</span>  {capitalizeWords(address)?.replace(/,+$/, "") || "N/A"}
+          {street2 && `, ${capitalizeWords(street2).replace(/,+$/, "")}`}
+          {city && `, ${capitalizeWords(city).replace(/,+$/, "")}`}
+          {zipCode && `, ${zipCode}`}
+        </p>
+        <br />
+        <p>
+          <span className="font-semibold">State or Country:</span> {stateOrCountryDescription || "N/A"}
+        </p>
+        <br />
+        <p>
+          <span className="font-semibold">State of Incorporation:</span> {stateOfIncorporation || "N/A"}
+        </p>
+        <br />
+        <p>
+          <span className="font-semibold">Date of Last Filing:</span> {formatDate(mostRecentFilingDate) || "N/A"}
+        </p>
+        <br />
+        <p>
+          <span className="font-semibold">Phone:</span> {phone || "N/A"}
+        </p>
+        <br />
+        <p>
+          <span className="font-semibold">Website:</span>{" "}
+          {website ? (
+            <><a
+                href={website}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 underline"
+              >
+                {website}
+              </a>
+                <span className="font-semibold text-sm"> please note we do not verify any external website linked on this page, click on links at your own risk</span>
+              </>
+          ) : (
+            "N/A"
+          )}
+        </p>
+      </div>
+      
+
+
+      {/* Right side */}
+      <div className="w-1/2 text-right text-xl p-6 rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold mb-4">Risk Report Feature Coming Soon To This Page!</h2>
       </div>
     </div>
   );
