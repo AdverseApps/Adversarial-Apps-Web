1ca5f780177604b3f49bbfb0caa7997d2949d393
chrisrod1307 <rodriguez.christian325@gmail.com>
2025-02-06 23:31:51 -0500

    feat: added close button to QR code for accesibility

--- adversarial_apps/src/components/QR.tsx
+++ adversarial_apps/src/components/QR.tsx
@@ -4,50 +4,51 @@ import QRCode from "react-qr-code";
 import Image from "next/image";
 
 interface QRCodeProps {
-    companyName: string;
-    cik: string;
+  companyName: string;
+  cik: string;
 }
 
 export const QRCodeComponent = (props: QRCodeProps) => {
-    const { cik, companyName } = props;
+  const { cik, companyName } = props;
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const openModal = () => setIsModalOpen(true);
-    const closeModal = () => setIsModalOpen(false);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
-    return (
-        <>
-            <button
-                onClick={openModal}
-                className="px-4 py-2 bg-blue-900 text-white rounded-md shadow-md hover:bg-blue-600 transition-colors flex items-center space-x-2 mt-2"
-            >
-                <p>QR Code</p>
-                <Image
-                    src="/QRCodeIcon.png"
-                    alt="QR Code Icon"
-                    width={30}
-                    height={30}
-                    className="invert"
-                />
-            </button>
+  return (
+    <>
+      <button
+        onClick={openModal}
+        className="px-4 py-2 bg-blue-900 text-white rounded-md shadow-md hover:bg-blue-600 transition-colors flex items-center space-x-2 mt-2"
+      >
+        <p>QR Code</p>
+        <Image
+          src="/QRCodeIcon.png"
+          alt="QR Code Icon"
+          width={30}
+          height={30}
+          className="invert"
+        />
+      </button>
 
-            {isModalOpen && (
-                <div
-                className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center resize-none"
-                onClick={closeModal}
-              >
-                <div
-                  className="bg-white p-4 rounded-lg"
-                  onClick={(e) => e.stopPropagation()}
-                >
-                  <div className="flex flex-col items-center break-words">
-                    <p className="text-black pb-2 text-center break-words max-w-full">{companyName}</p>
-                    <QRCode value={`https://adversarialapps.com/company/` + cik} className="rounded-lg" />
-                  </div>
-                </div>
-              </div>
-            )}
-        </>
-    );
+      {isModalOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center resize-none"
+          onClick={closeModal}
+        >
+          <div
+            className="bg-white p-4 rounded-lg flex flex-col items-center"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="flex flex-col items-center break-words">
+              <p className="text-black pb-2 text-center break-words max-w-full">{companyName}</p>
+              <QRCode value={`https://adversarialapps.com/company/` + cik} className="rounded-lg" />
+            </div>
+            <button className="bg-black text-white px-3 py-1 mt-4 rounded hover:bg-gray-800 transition-colors" onClick={closeModal}>Close</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
