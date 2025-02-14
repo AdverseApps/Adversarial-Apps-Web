e75e1c64acbc27b1915fde815dfa663b090f5b3d
chrisrod1307 <rodriguez.christian325@gmail.com>
2025-02-13 19:39:43 -0500

    Added stars to favorite button

--- adversarial_apps/src/components/FavoriteButton.tsx
+++ adversarial_apps/src/components/FavoriteButton.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from "next/link";
+import Image from "next/image";
 import { useState } from "react";
 
 interface Props {
@@ -13,7 +14,7 @@ export const FavoriteButton = (props: Props) => {
     const { cik, username, favorites } = props;
     const [isFavorite, setIsFavorite] = useState(favorites.includes(cik));
     const [showLoginMessage, setShowLoginMessage] = useState(false);
-    
+
     // function for handling when the user clicks 'add to favorites' button
     const handleClick = async () => {
         // Checking if the user is logged in
@@ -29,7 +30,7 @@ export const FavoriteButton = (props: Props) => {
                 const data = await response.json();
                 console.log(data.message);
 
-                if (response.ok){
+                if (response.ok) {
                     setIsFavorite((prev) => !prev);
                 }
             } catch (error) {
@@ -45,14 +46,21 @@ export const FavoriteButton = (props: Props) => {
     return (
         <div>
             <button onClick={handleClick} className="px-4 py-2 bg-blue-900 text-white rounded-md shadow-md hover:bg-blue-600 transition-colors flex items-center space-x-2 mt-2">
-                {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+                <Image
+                    src={isFavorite ? "/FilledStar.png" : "/EmptyStar.png"}
+                    alt="Favorite Icon"
+                    width={30}
+                    height={20}
+                    className={!isFavorite ? "invert" : ""}
+                />
+                <span>{isFavorite ? "Remove from Favorites" : "Add to Favorites"}</span>
             </button>
 
             {/* Message if not logged in */}
             {showLoginMessage && (
                 <div className="bg-gray-700 text-white px-4 py-2 rounded-md shadow-md mt-2">
-                Please <Link href="/login" className="underline text-blue-400 hover:text-blue-300 transition">log in</Link> or <Link href="/signup" className="underline text-blue-400 hover:text-blue-300 transition">sign up</Link> to add favorites.
-            </div>
+                    Please <Link href="/login" className="underline text-blue-400 hover:text-blue-300 transition">log in</Link> or <Link href="/signup" className="underline text-blue-400 hover:text-blue-300 transition">sign up</Link> to add favorites.
+                </div>
             )}
         </div>
     )
