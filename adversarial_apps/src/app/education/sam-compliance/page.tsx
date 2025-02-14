7ce3b8905da93cefec8a93a1daa79c53b81179a4
chrisrod1307 <rodriguez.christian325@gmail.com>
2025-02-13 20:06:45 -0500

    making the whole 'Back to Edu Hub' be clickable

--- adversarial_apps/src/app/education/sam-compliance/page.tsx
+++ adversarial_apps/src/app/education/sam-compliance/page.tsx
@@ -25,10 +25,12 @@ export default function page(){
 
             {/* copy paste the below button(s) for each subpage */}
             <div className="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
-                <button className="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded"
+                <Link href = "/education">
+                    <button className="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded"
                         aria-label="Back to Education Hub">
-                    <Link href = "/education">Back to Education Hub</Link>
-                </button>
+                        Back to Education Hub
+                    </button>
+                </Link>
             </div>
         </main>
     );
