1b54e97ad0d57ea7afd4b1cd274df4a9903cef55
Oluwadamilola Ogboja <134186195+DamiOgboja@users.noreply.github.com>
2025-02-18 23:46:28 -0500

    Update navbar.tsx

--- adversarial_apps/src/components/navbar.tsx
+++ adversarial_apps/src/components/navbar.tsx
@@ -17,12 +17,54 @@ const EducationSubNavBar = ({ isSubMenuOpen }: { isSubMenuOpen: boolean }) => {
       } bg-blue-800 text-white rounded-lg p-4 mt-2 shadow-lg`}
     >
       <ul className="flex space-x-6">
-        <li><Link href="/education/cfr-title-15" className="block py-1 hover:bg-blue-700 px-2 whitespace-nowrap">CFR Title 15</Link></li>
-        <li><Link href="/education/sam-compliance" className="block py-1 hover:bg-blue-700 px-2 whitespace-nowrap">SAM Compliance</Link></li>
-        <li><Link href="/education/sbir-due-diligence" className="block py-1 hover:bg-blue-700 px-2 whitespace-nowrap">SBIR Due Diligence</Link></li>
-        <li><Link href="/education/cmmc" className="block py-1 hover:bg-blue-700 px-2 whitespace-nowrap">CMMC</Link></li>
-        <li><Link href="/education/foci" className="block py-1 hover:bg-blue-700 px-2 whitespace-nowrap">FOCI</Link></li>
-        <li><Link href="/education/resources" className="block py-1 hover:bg-blue-700 px-2 whitespace-nowrap">Resources</Link></li>
+        <li>
+          <Link
+            href="/education/cfr-title-15"
+            className="block py-1 hover:bg-blue-700 px-2 whitespace-nowrap"
+          >
+            CFR Title 15
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/education/sam-compliance"
+            className="block py-1 hover:bg-blue-700 px-2 whitespace-nowrap"
+          >
+            SAM Compliance
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/education/sbir-due-diligence"
+            className="block py-1 hover:bg-blue-700 px-2 whitespace-nowrap"
+          >
+            SBIR Due Diligence
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/education/cmmc"
+            className="block py-1 hover:bg-blue-700 px-2 whitespace-nowrap"
+          >
+            CMMC
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/education/foci"
+            className="block py-1 hover:bg-blue-700 px-2 whitespace-nowrap"
+          >
+            FOCI
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/education/resources"
+            className="block py-1 hover:bg-blue-700 px-2 whitespace-nowrap"
+          >
+            Resources
+          </Link>
+        </li>
       </ul>
     </div>
   );
