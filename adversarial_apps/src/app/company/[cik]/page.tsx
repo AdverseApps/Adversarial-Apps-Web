5a3a885fc90af8559bcbd666275aebfc9fefe683
Oluwadamilola Ogboja <134186195+DamiOgboja@users.noreply.github.com>
2025-02-27 04:47:03 -0500

    added a reviewer check and verifycompany button
    page now checks for reviewer status first and then if the user is a reviewer adds the verifyButton

--- adversarial_apps/src/app/company/[cik]/page.tsx
+++ adversarial_apps/src/app/company/[cik]/page.tsx
@@ -3,9 +3,11 @@ import {
   getUsername,
   getFavorites,
   getRiskScore,
+  verifyUser,
 } from "@/app/lib/data";
 import { QRCodeComponent } from "@/components/QR";
 import { FavoriteButton } from "@/components/FavoriteButton";
+import VerifyButton from "@/components/VerifyButton";
 
 interface CompanyDetailsProps {
   params: { cik: string };
@@ -75,6 +77,16 @@ export default async function page({ params }: CompanyDetailsProps) {
     console.error("Error in getting risk score:", error);
   }
 
+  let reviewerData;
+  try{
+     reviewerData = await verifyUser();
+     if (!reviewerData || typeof reviewerData !== "object") {
+      throw new Error("Invalid response from VerifyReviewer.");
+    }
+  } catch (error) {
+    console.error("Error in verifying user:", error);
+  }
+
   const {
     name,
     formerNames,
@@ -99,7 +111,9 @@ export default async function page({ params }: CompanyDetailsProps) {
   const { favorites } = await getFavorites(username);
   console.log(username);
   console.log(favorites);
+// *** Server-side check for reviewer role ***
 
+// reviewerData will be an object { username, role } or null
   return (
     <div>
       <div className="flex mt-6 ">
@@ -184,6 +198,10 @@ export default async function page({ params }: CompanyDetailsProps) {
 
           <QRCodeComponent companyName={name} cik={cik} />
           <FavoriteButton cik={cik} username={username} favorites={favorites} />
+          {/* Render the "Verify Company" button only if user is a reviewer */}
+          {reviewerData && reviewerData.role === "true" && (
+            <VerifyButton cik={cik} />
+          )}
         </div>
 
         {/* Right side */}
@@ -203,6 +221,7 @@ export default async function page({ params }: CompanyDetailsProps) {
           )}
         </div>
       </div>
+       
       <footer>
         {/* Properly Citing the SEC*/}
         <p>
