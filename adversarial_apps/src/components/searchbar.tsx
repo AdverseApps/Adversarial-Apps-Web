81d470b59f337cda5e815b0452c5a81f83b90199
Oluwadamilola Ogboja <134186195+DamiOgboja@users.noreply.github.com>
2025-03-11 13:22:40 -0400

    searchbar added pagination +formatting

--- adversarial_apps/src/components/searchbar.tsx
+++ adversarial_apps/src/components/searchbar.tsx
@@ -1,6 +1,6 @@
-'use client';
-import { useSearchParams, useRouter } from 'next/navigation';
-import { useState, useEffect, Suspense } from 'react';
+"use client";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useState, useEffect, Suspense } from "react";
 
 export default function SearchBar({ placeholder }: { placeholder: string }) {
   return (
@@ -10,18 +10,22 @@ export default function SearchBar({ placeholder }: { placeholder: string }) {
   );
 }
 
-function SearchBarContent({ placeholder }: {placeholder: string}) {
-    
-    const searchParams = useSearchParams();
-    const replace = useRouter();
-    const [searchTerm, setSearchTerm] = useState('');
-    const [results, setResults] = useState<{ name: string; cik: string }[]>([]);
-    const [showDropdown, setShowDropdown] = useState(false);
+function SearchBarContent({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const replace = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState<{ name: string; cik: string }[]>([]);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
+  const resultsPerPage = 10;
+  const paginatedResults = results.slice(
+    (currentPage - 1) * resultsPerPage,
+    currentPage * resultsPerPage
+  );
 
-
-    useEffect(() => {
-      const delayDebounceFn = setTimeout(() => {
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
       if (searchTerm) {
         console.log("Searching for:", searchTerm);
         fetchResults(searchTerm);
@@ -32,116 +36,126 @@ function SearchBarContent({ placeholder }: {placeholder: string}) {
       }
     }, 300); // Delay of 300ms
     return () => clearTimeout(delayDebounceFn); //debouncing to reduce unnecessary requests to the server
-    }, [searchTerm]);
+  }, [searchTerm]);
 
-    async function fetchResults(query: string) {
-      //CAN POSSIBLLY REMOVE FOR THE LIB DATA ADDITION
-        // Data will hold what will be given in the API body. [query] is what is typed in the box
-        console.log('Fetching CIK number...');
-        const data = { action: "obtain_cik_number", search_term: query };
-        const response = await fetch('/api/call-python-api', {
-          method: 'Post',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data)
-        });
-        if (response.status === 200) {
-          const result = await response.json();
-          console.log(result);
-  
-          const companies = result.companies.map((company: { "Company Name": string, "CIK": string }) => ({
-              name: company["Company Name"],
-              cik: company["CIK"]
-          }));
-          
-          setResults(companies);
-      }
-/*
-LIB DATA ATTEMPT, WILL LOOK AT MORE LATER -Dami
-      try {
-        const companies = await FetchCIKnumber(query); // Fetch data using the imported function
-        if (companies) {
-          setResults(companies);
-        } else {
-          setResults([]); // Handle cases where no results are returned
-        }
-      } catch (error) {
-        console.error('Error fetching results:', error);
-        setResults([]); // Reset results on error
-      }
-        */
-    }
-    
+  async function fetchResults(query: string) {
+    //CAN POSSIBLLY REMOVE FOR THE LIB DATA ADDITION
+    // Data will hold what will be given in the API body. [query] is what is typed in the box
+    console.log("Fetching CIK number...");
+    const data = { action: "obtain_cik_number", search_term: query };
+    const response = await fetch("/api/call-python-api", {
+      method: "Post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (response.status === 200) {
+      const result = await response.json();
+      console.log(result);
 
-      function handleSearch(term: string) {
-        if (term) {
-          replace.push(`/search?query=${term}`);
-        }
-      }
-      
-      function sanitizeInput(input: string): string {
-        // removes special characters like <, >, ".
-        return input.replace(/<|>/g, "");
-      }
-
-      return (
-
-        // Changing this to just "flex", does not display the results correctly.
-        <div className="relative flex">
-          <label htmlFor="search" className="sr-only">
-            Search
-          </label>
-
-          <input
-            id = "search"
-            //className="block w-full rounded-3xl border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500 text-gray-500"
-            className={`block w-full border border-gray-300 py-2 pl-4 pr-4 text-sm text-gray-500 placeholder-gray-500 focus:outline-none focus:border-gray-400 ${
-              showDropdown ? 'rounded-t-3xl rounded-b-none' : 'rounded-3xl'
-            }`}
-            
-            placeholder={placeholder}
-            value ={searchTerm}
-            onChange={(e) => setSearchTerm(sanitizeInput(e.target.value))}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-              handleSearch(e.currentTarget.value);
-              setShowDropdown(false); // Hide dropdown on Enter
-            }
-            }}
-            defaultValue={searchParams.get('query')?.toString()}
-          />
-
-          {showDropdown && results.length > 0 && (
-            <ul className="flex-none absolute w-full rounded-b-3xl border border-t-0 border-gray-300 bg-white shadow-lg text-gray-500"
-            style={{ top: '100%' }}>
-            {results.map((result, index) => (
-              <li
-                key={index}
-                className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                tabIndex={0}
-                onClick={() => {
-                  //REPLACE WITH HAVING THEM REDIRECTED TO RESULTS PAGE FOR THE ENTRY HERE
-                  console.log('Redirecting to:', `/company/${result.cik}`); //debugging
-                  replace.push(`/company/${result.cik}`);
-                  setShowDropdown(false); // Hide dropdown after selection
-                }}
-                onKeyDown={(e) => {
-                  if (e.key === 'Enter') {
-                    console.log('Redirecting to:', `/company/${result.cik}`); //debugging
-                    replace.push(`/company/${result.cik}`);
-                    setShowDropdown(false);
-                  }}
-              }
-              >
-              {result.name}
-              </li>
-              ))
-            }
-            </ul>
-            )
-          }
-
-        </div>
+      const companies = result.companies.map(
+        (company: { "Company Name": string; CIK: string }) => ({
+          name: company["Company Name"],
+          cik: company["CIK"],
+        })
       );
+
+      setResults(companies);
+      setCurrentPage(1);
     }
+  }
+
+  function handleSearch(term: string) {
+    if (term) {
+      replace.push(`/search?query=${term}`);
+    }
+  }
+
+  function nextPage() {
+    if (currentPage * resultsPerPage < results.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  function prevPage() {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  function sanitizeInput(input: string): string {
+    // removes special characters like <, >, ".
+    return input.replace(/<|>/g, "");
+  }
+
+  return (
+    // Changing this to just "flex", does not display the results correctly.
+    <div className="relative flex">
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+
+      <input
+        id="search"
+        //className="block w-full rounded-3xl border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500 text-gray-500"
+        className={`block w-full border border-gray-300 py-2 pl-4 pr-4 text-sm text-gray-500 placeholder-gray-500 focus:outline-none focus:border-gray-400 ${
+          showDropdown ? "rounded-t-3xl rounded-b-none" : "rounded-3xl"
+        }`}
+        placeholder={placeholder}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(sanitizeInput(e.target.value))}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSearch(e.currentTarget.value);
+            setShowDropdown(false); // Hide dropdown on Enter
+          }
+        }}
+        defaultValue={searchParams.get("query")?.toString()}
+      />
+
+      {showDropdown && results.length > 0 && (
+        <ul
+          className="flex-none absolute w-full rounded-b-3xl border border-t-0 border-gray-300 bg-white shadow-lg text-gray-500"
+          style={{ top: "100%" }}
+        >
+          {paginatedResults.map((result, index) => (
+            <li
+              key={index}
+              className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+              tabIndex={0}
+              onClick={() => {
+                replace.push(`/company/${result.cik}`);
+                setShowDropdown(false);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  replace.push(`/company/${result.cik}`);
+                  setShowDropdown(false);
+                }
+              }}
+            >
+              {result.name}
+            </li>
+          ))}
+          <div className="flex justify-between px-4 py-2 border-t border-gray-300">
+            <button
+              onClick={prevPage}
+              disabled={currentPage === 1}
+              className="text-sm text-gray-500 disabled:opacity-50"
+            >
+              &lt; Previous
+            </button>
+            <button
+              onClick={nextPage}
+              disabled={currentPage * resultsPerPage >= results.length}
+              className="text-sm text-gray-500 disabled:opacity-50"
+            >
+              Next &gt;
+            </button>
+          </div>
+        </ul>
+      )}
+    </div>
+  );
+}
