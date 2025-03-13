d799deee5648224cd8a45224471b040a9f987ab2
Chris Gagnier <143040358+mechamancy@users.noreply.github.com>
2025-03-13 19:37:27 -0400

    first draft of search page disclaimer

--- adversarial_apps/src/app/search/page.tsx
+++ adversarial_apps/src/app/search/page.tsx
@@ -49,7 +49,8 @@ export default function Page() {
           </p>
 
           <p className="text-md mt-4">
-            <p><u>We do not track user searches with this tool.</u> </p>
+            <p><u>We do not track user searches with this tool.</u> We value user privacy and will not save anything without your permission. We only store</p>
+            <p>user data for login credentials and user-favorited companies. For website demonstration inquiries, please email: adversarialapps@gmail.com</p>
           </p>
         </section>
 
