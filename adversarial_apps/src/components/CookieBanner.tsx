9619fecdb042bc9388dff7551cf457b984c14041
James Allen <master.james.allen@gmail.com>
2025-03-25 12:07:31 -0400

    eslint fix

--- adversarial_apps/src/components/CookieBanner.tsx
+++ adversarial_apps/src/components/CookieBanner.tsx
@@ -12,7 +12,7 @@ export default function CookieBanner() {
       buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
       expires={150}
     >
-      We do not track user searches with cookies. Cookies are exclusively used for User Favoriting, nothing else is tracked. If you want user functionality to work as intended, please allow cookies by pressing: "Accept"
+      We do not track user searches with cookies. Cookies are exclusively used for User Favoriting, nothing else is tracked. If you want user functionality to work as intended, please allow cookies by pressing: &quot;Accept&quot;
     </CookieConsent>
   );
 }
