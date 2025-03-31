268d30fd044dd379c820fbc78c172e0d9cbb39f5
Conner Harbaugh <savvydolphin77@gmail.com>
2025-03-31 02:43:26 -0400

    Fixed Changes Requested: Interface Transaction Updated transaction code information to "" the types of symbols we use. Added the transaction_code to the interface for ensuring correct type

--- adversarial_apps/src/components/RecentOwnership.tsx
+++ adversarial_apps/src/components/RecentOwnership.tsx
@@ -13,6 +13,7 @@ interface FilingReport {
     form: string;
     issuer: string;
     reporter: string;
+    transaction_code: string;
 }
 
 export const RecentOwnership = (props: Props) => {
@@ -112,9 +113,9 @@ export const RecentOwnership = (props: Props) => {
                 The letter relates to the exact type of transaction that has occurred. For covnince we summed up the transaction codes and provided the key:
             </p>
             <ul className="list-disc ml-5">
-                <li>If the transaction code letter has a + that means the Receiver has obtained more control over the company.</li>
-                <li>If the transaction code letter has a - that means the Receiver has lost or given up control over the company.</li>
-                <li>If the transaction code letter has a ~ that means the Receiver has not changed their ownership of the company.</li>
+                <li>If the transaction code letter has a "+" that means the Receiver has obtained more control over the company.</li>
+                <li>If the transaction code letter has a "-" that means the Receiver has lost or given up control over the company.</li>
+                <li>If the transaction code letter has a "~" that means the Receiver has not changed their ownership of the company.</li>
             </ul>
             <br />
             {error && <p className="text-red-500">Error: {error}</p>}
