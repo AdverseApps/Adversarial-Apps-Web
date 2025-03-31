30d97e190cd497c3fe76dca734a7c107dbb41ff5
chrisrod1307 <rodriguez.christian325@gmail.com>
2025-03-31 18:20:29 -0400

    Fixing Pipeline

--- adversarial_apps/src/app/about/page.tsx
+++ adversarial_apps/src/app/about/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import Link from "next/link";
 
-export default function page() {
+export default function Page() {
     const [isAdversarialAppsOpen, setIsAdversarialAppsOpen] = useState(false);
     const [isAssessmentOpen, setIsAssessmentOpen] = useState(false);
     const [isRiskLevelsOpen, setIsRiskLevelsOpen] = useState(false);
