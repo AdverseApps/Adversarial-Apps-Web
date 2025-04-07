e40cf17715c3834ddcb31949cc9661a88b732b1d
Chris Gagnier <143040358+mechamancy@users.noreply.github.com>
2025-04-07 12:20:23 -0400

    perform proper checks (#209)
    included case where user is not logged in

--- adversarial_apps/src/app/company/[cik]/page.tsx
+++ adversarial_apps/src/app/company/[cik]/page.tsx
@@ -162,7 +162,7 @@ export default async function page({ params }: CompanyDetailsProps) {
             </h2>
 
             {/* Split div into reviewer/user conditions */}
-            {reviewerData && reviewerData.role === "true" && (
+            {((!reviewerData) || (reviewerData && reviewerData.role === "true")) && (
               <div className="flex items-center space-x-2">
                 {" "}
                 {/* Wrap buttons */}
